b5707fbedff51b17ef0fb59902431975247d19ee
Sylvain Juge <syl20j@gmail.com>
2013-10-19 19:07:43 +0200

    fix build on linux
    removing 1st slash was just a bad idea, converting URL to URI allows to
    avoid such bad platform-specific behavior

--- src/test/java/sylvain/juge/memoryfs/util/ProjectPathFinder.java
+++ src/test/java/sylvain/juge/memoryfs/util/ProjectPathFinder.java
@@ -1,5 +1,6 @@
 package sylvain.juge.memoryfs.util;
 
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,19 +14,23 @@ public final class ProjectPathFinder {
     /**
      * Searches folder with name {@code rootName} starting from current classpath root, then
      * upper until it reaches root or reaches first folder with this name.
-     * @param rootName
+     * @param rootName project folder name
      * @return null if not found, path to folder otherwise
      */
-    public static Path getFolder(String rootName){
+    public static Path getFolder(String rootName) {
         URL classpathRoot = ProjectPathFinder.class.getClassLoader().getResource("");
-        System.out.println(classpathRoot);
-        // replacing first slash is mandatory on windows
-        Path folder = Paths.get(classpathRoot.getFile().replaceFirst("/", ""));
+        if (null == classpathRoot) {
+            throw new IllegalStateException("unable to retrieve classpath root");
+        }
+        Path folder;
+        try {
+            folder = Paths.get(classpathRoot.toURI());
+        } catch (URISyntaxException e) {
+            throw new IllegalStateException(e);
+        }
         while (null != folder && !rootName.equals(folder.getFileName().toString())) {
-            System.out.println(folder);
             folder = folder.getParent();
         }
-        System.out.println(folder);
         return null != folder ? folder : null;
     }
 }
